635682db5ce94a058f87889a0a8c54938997e6a1
david <dplackner@gmail.com>
2025-07-26 15:36:06 -0700

    Fix build output directory for Vite

--- push-to-github.sh
+++ push-to-github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace YOUR_GITHUB_USERNAME with your actual GitHub username
-GITHUB_USERNAME="YOUR_GITHUB_USERNAME"
+GITHUB_USERNAME="dlackner"
 
 git remote add origin https://github.com/$GITHUB_USERNAME/sentiment-trader-pro.git
 git push -u origin main
